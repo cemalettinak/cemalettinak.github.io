1900768126d2675f96479353cf514e4ffcc5f81b
cemalettinak <135587443+cemalettinak@users.noreply.github.com>
2026-03-17 21:13:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,40 +130,6 @@
     });
 });
 
-    const zzSlides = document.querySelectorAll(".zz-item");
-const zzDotsContainer = document.getElementById("zzDots");
 
-let zzIndex = 0;
-
-// dot oluştur
-zzSlides.forEach((_, i) => {
-    const dot = document.createElement("span");
-    dot.addEventListener("click", () => goToSlide(i));
-    zzDotsContainer.appendChild(dot);
-});
-
-const zzDots = zzDotsContainer.querySelectorAll("span");
-
-function showSlide(index) {
-    zzSlides.forEach(s => s.classList.remove("active"));
-    zzDots.forEach(d => d.classList.remove("active"));
-
-    zzSlides[index].classList.add("active");
-    zzDots[index].classList.add("active");
-}
-
-function nextSlide() {
-    zzIndex = (zzIndex + 1) % zzSlides.length;
-    showSlide(zzIndex);
-}
-
-function goToSlide(i) {
-    zzIndex = i;
-    showSlide(zzIndex);
-}
-
-// başlat
-showSlide(zzIndex);
-setInterval(nextSlide, 3000);
 })(jQuery);
 
